07dd88b4a52dc73c4ecbaa1fc6a67d2c8eb29e50
Margit Robert <marrob50hz@gmail.com>
2022-01-19 10:30:08 +0100

    first

---
+++ Core/Inc/main.h
@@ -0,0 +1,103 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+#define DEVICE_DEBUG_LEVEL    3
+
+#if (DEVICE_DEBUG_LEVEL > 0)
+#define  DeviceUsrLog(...)  {printf(__VA_ARGS__);\
+                             printf("\r\n");}
+#else
+#define DeviceUsrLog(...)
+#endif
+
+#if (DEVICE_DEBUG_LEVEL > 1)
+
+#define  DeviceErrLog(...)  {printf("ERROR.DEVICE:") ;\
+                             printf(__VA_ARGS__);\
+                             printf("\r\n");}
+#else
+#define DeviceErrLog(...)
+#endif
+
+#if (DEVICE_DEBUG_LEVEL > 2)
+#define  DeviceDbgLog(...)  {printf("DEBUG.DEVICE:") ;\
+                             printf(__VA_ARGS__);\
+                             printf("\r\n");}
+#else
+#define DeviceDbgLog(...)
+#endif
+
+#define DEVICE_FW           0x0000
+
+#define DEVICE_MNF          "CoreAudio"
+#define DEVICE_NAME_SIZE    32
+#define DEVICE_FW_SIZE      sizeof(DEVICE_FW)
+#define DEVICE_PCB_SIZE     sizeof(DEVICE_PCB)
+#define DEVICE_MNF_SIZE     sizeof(DEVICE_MNF)
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define LIVE_LED_Pin GPIO_PIN_13
+#define LIVE_LED_GPIO_Port GPIOC
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
