8a4286e197485eef02726e25a2e58a1ef4750c16
Margit Robert <marrob50hz@gmail.com>
2022-02-05 16:21:28 +0100

    update

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -666,7 +666,8 @@ void LiveLedOff(void)
 /* printf --------------------------------------------------------------------*/
 int _write(int file, char *ptr, int len)
 {
-  HAL_UART_Transmit(&huart1, (uint8_t*)ptr, len, 100);
+  //HAL_UART_Transmit(&huart1, (uint8_t*)ptr, len, 100);
+
   return len;
 }
 
